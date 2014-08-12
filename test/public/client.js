cfbd4fc1ff7d7c405ef06486e2c91e74d13d64e0
Felipe Triana <ftrianakast@gmail.com>
2014-08-12 13:36:04 -0500

    Last changes for test with 21212

--- test/public/client.js
+++ test/public/client.js
@@ -1,12 +1,19 @@
 var AentropicoExecutor = require('ae');
 var dataAppExecutor = new AentropicoExecutor();
+var dataAppId = "rxghd";
+
 
 dataAppExecutor.buildDataPackage({
     "tableA": "https://s3.amazonaws.com/aecantor/2ha2l.csv"
-}, "s2luq", function(dataPackage) {
-    dataAppExecutor.runDataApp(dataPackage, "s2luq", function(job) {
-        dataAppExecutor.getResult(job, function(res) {
-            console.log(res);
-        });
+}, "rxghd", function(dataPackage) {
+    dataAppExecutor.runDataApp(dataPackage, "rxghd", function(job) {
+        var myInterval = setInterval(function() {
+            dataAppExecutor.getResult(job, function(res) {
+                if (res.length !== 0) {
+                    console.log(res);
+                    window.clearInterval(myInterval);
+                }
+            });
+        }, 3000);
     });
 });
